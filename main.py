33c6e11aba303dceb3d3a01ffe638d461c9f4f62
7heBlackLand <janmamunegypt@gmail.com>
2025-10-30 21:29:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 
-class MyApp(App):
+class HelloApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', padding=40, spacing=20)
-        self.label = Label(text="Hello from Kivy!", font_size=24)
-        button = Button(text="Click Me", font_size=20, size_hint=(1, 0.3))
-        button.bind(on_press=self.on_button_click)
-        layout.add_widget(self.label)
-        layout.add_widget(button)
-        return layout
-
-    def on_button_click(self, instance):
-        self.label.text = "Button Clicked!"
+        return Label(text="Hello from Kivy + Buildozer + GitHub Actions!", font_size='20sp')
 
 if __name__ == "__main__":
-    MyApp().run()
+    HelloApp().run()
