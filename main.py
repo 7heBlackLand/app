3a5396e8b65ecb3140d444b8aad1157860d87299
7heBlackLand <janmamunegypt@gmail.com>
2025-10-30 23:55:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class HelloApp(App):
+class MyApp(App):
     def build(self):
-        return Label(text="Hello from Kivy + Buildozer + GitHub Actions!", font_size='20sp')
+        return Label(text="হ্যালো, এটি আমার প্রথম Android অ্যাপ!")
 
 if __name__ == "__main__":
-    HelloApp().run()
+    MyApp().run()
